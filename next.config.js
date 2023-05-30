40336dab811c61e941a069c07763251cd44c3a55
Augusto Rodrigues <augustofeliperodrigues1999@gmail.com>
2023-05-30 07:58:06 -0300

    [Augusto] fix(config): Implemented a webpack config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['plugin-storage.nyc3.digitaloceanspaces.com']
-  }
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
 }
 
 module.exports = nextConfig
