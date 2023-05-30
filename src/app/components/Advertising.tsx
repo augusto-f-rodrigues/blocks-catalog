7a97639b0084bfa37e5cf8a38fd4f3c8483f05a4
Augusto Rodrigues <augustofeliperodrigues1999@gmail.com>
2023-05-30 09:51:55 -0300

    [Augusto] feat: Link for footer link, adjusting the width of advertising component

--- src/app/components/Advertising.tsx
+++ src/app/components/Advertising.tsx
@@ -5,7 +5,7 @@ export default function Advertising() {
   return (
     <>
       <div
-        className={`sticky top-0 hidden h-[67px] w-screen flex-row items-center justify-center gap-[18px] bg-cinza-800 p-6 text-[15px] text-lg lg:flex`}
+        className={`sticky top-0 hidden h-[67px] w-full flex-row items-center justify-center gap-[18px] bg-cinza-800 p-6 text-[15px] text-lg lg:flex`}
       >
         <span className="text-white">
           Não limite sua criatividade, junte-se a familia Blocks por apenas{" "}
--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,11 +1,21 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <footer className="flex h-auto items-center justify-start md:justify-center bg-cinza-100 p-4 md:h-[65px] md:p-0">
+    <footer className="flex h-auto items-center justify-start bg-cinza-100 p-4 md:h-[65px] md:justify-center md:p-0">
       <ul className="flex flex-col gap-2 md:flex-row md:gap-6">
-        <li>Sobre</li>
-        <li>FAQ</li>
-        <li>Termos de uso</li>
-        <li>Política de privacidade</li>
+        <li>
+          <Link href={"#"}>Sobre</Link>
+        </li>
+        <li>
+          <Link href={"#"}>FAQ</Link>
+        </li>
+        <li>
+          <Link href={"#"}>Termos de uso</Link>
+        </li>
+        <li>
+          <Link href={"#"}>Política de privacidade</Link>
+        </li>
       </ul>
     </footer>
   );
